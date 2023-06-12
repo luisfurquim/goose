ee4fb49198b60776de2745ae4926fc4c2470dafc
Luis Otavio de Colla Furquim <luisfurquim@gmail.com>
2023-06-12 01:34:35 -0300

    @{bugfix}Trying to make it compile to windows.

--- syslog_linux.go
+++ syslog_linux.go
@@ -1,1 +1,39 @@
-syslog_darwin.go
+package goose
+
+import (
+   "os"
+   "log"
+   "strings"
+   "log/syslog"
+)
+
+// SyslogGoose is a wrapper type around *syslog.Writer to ensure a syslogger that satisfies the io.Writer inrterface
+type SyslogGoose struct {
+   W *syslog.Writer
+}
+
+// Write is the method that satisfies the io.Writer inrterface
+func (ng SyslogGoose) Write(b []byte) (int, error) {
+   return ng.W.Write(b)
+}
+
+// UseSyslogNet redirects the log output from os.Stderr to the system logger
+// connecting to it via network.
+func UseSyslogNet(proto string, srv string, priority syslog.Priority) error {
+   var logOutput     SyslogGoose
+   var binParts    []string
+   var binName       string
+   var err           error
+
+   binParts = strings.Split(os.Args[0],string([]byte{os.PathSeparator}))
+   binName  = binParts[len(binParts)-1]
+
+   logOutput.W, err = syslog.Dial(proto, srv, priority, binName)
+   if err != nil {
+      return err
+   }
+   log.SetOutput(logOutput)
+   log.SetFlags(0)
+   return nil
+}
+
